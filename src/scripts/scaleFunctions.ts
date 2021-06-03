b6e9c534a6ca29b20a734d22eee1b7bbe1b1df24
Stjepan Mrgnaić <stjepan.mrganic@gmail.com>
2021-06-03 23:42:04 +0200

    Started testing dataset rendering

--- src/scripts/scaleFunctions.ts
+++ src/scripts/scaleFunctions.ts
@@ -2,6 +2,13 @@ import { scaleBand } from "d3-scale"
 import { decades } from "./constants/circle"
 import { svgWidth } from "./constants/svg"
 
+const data = require("../../public/data/eurovisionData.json")
+
 export const circleScale = scaleBand()
   .domain(decades.map(String))
   .range([100, svgWidth / 2])
+
+export const xScale = scaleBand()
+  .range([0, 2 * Math.PI])
+  .align(0)
+  .domain(data.map((singleElement: any) => singleElement.id))
